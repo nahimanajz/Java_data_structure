1bc8b2cd7ba146cb67fff5f38a2714410997b6a0
nahimanajz <jazzonahimana@gmail.com>
2022-06-13 13:37:02 +0200

    reversing an array

---
+++ src/maharishi/ReverseArray.java
@@ -0,0 +1,21 @@
+package maharishi;
+
+import java.util.Arrays;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] numbers = new int[] {12, 3, 4,5 ,7};
+        int startIndex =0, endIndex = numbers.length -1;
+        while(startIndex < endIndex) {
+            reverse(numbers, startIndex, endIndex);
+            startIndex ++;
+            endIndex --;
+        }
+        System.out.println(Arrays.toString(numbers));
+    }
+    public static void reverse(int[] numbers, int start, int end){
+        int temp = numbers[start];
+        numbers[start] =  numbers[end];
+        numbers[end] = temp;
+    }
+}
