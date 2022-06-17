db5f06a4e849bd21e8711101028b9f819219694e
nahimanajz <jazzonahimana@gmail.com>
2022-06-17 21:49:22 +0200

    find element in2D array

---
+++ src/maharishi/SearchInTwoMatrix.java
@@ -0,0 +1,29 @@
+package maharishi;
+import java.util.Arrays;
+
+public class SearchInTwoMatrix {
+    public static void main(String[] args) {
+   int[][] matrix = {
+           {10, 20, 30, 40},
+           {15, 25, 35, 45},
+           {28, 29, 37, 49},
+           {33, 34, 38, 50}
+   };
+   int target =49;
+        System.out.println(Arrays.toString(find(matrix, target)));
+    }
+    public static  int[] find(int[][] matrix, int target){
+        int row = 0;
+        int col = matrix.length -1;
+        while(row < matrix.length && col >= 0) {
+            if(matrix[row][col] == target) {
+                return new int[] {row, col};
+            } else if (matrix[row][col]< target) {
+                row++;
+            }else{
+                col --;
+            }
+        }
+      return new int[]{-1, -1};
+    }
+}
