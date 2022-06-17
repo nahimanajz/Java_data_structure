c05958443a7e699e12e40644b522944fcb06925e
nahimanajz <jazzonahimana@gmail.com>
2022-06-17 09:48:27 +0200

    add find index

--- src/maharishi/CountRotation.java
+++ src/maharishi/CountRotation.java
@@ -3,7 +3,7 @@ package maharishi;
 
 public class CountRotation {
     public static void main(String[] args) {
-    int[] arr = {4,5,6,7,0,1, 2};
+    int[] arr = {4,5,6,7,8,19, 12,0};
         System.out.println(rotationCounter(arr));
     }
 
@@ -17,15 +17,20 @@ public class CountRotation {
         int start = 0, end = arr.length -1;
         while(start <= end) {
             int mid = start + (end -start) /2;
-            if(arr[mid] < arr[mid + 1]) {
+           // 4 cases for finding pivot
+            if(mid < end && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            if (mid > start && arr[mid] < arr[mid -1]) {
+                return mid - 1;
+            }
+            if(arr[mid] <= arr[start]) {
+                end = mid -1;
+            }else {
                 start = mid +1;
-            }else if (arr[mid] > arr[mid]) {
-                end = mid - 1;
-            } else {
-                return  mid;
             }
         }
-        return end; // since end <= start then return any of these two variable
+        return -1; // since end <= start then return any of these two variable
     }
 
 }
