43219c445bdaed3005944ae3f4c8a4c3589ce93b
nahimanajz <jazzonahimana@gmail.com>
2022-06-30 12:35:38 +0200

    (recursion): Get indexes where target occurs

--- src/maharishi/recursions/arrays/FindElement.java
+++ src/maharishi/recursions/arrays/FindElement.java
@@ -1,12 +1,29 @@
 package maharishi.recursions.arrays;
+
+import java.util.ArrayList;
+
 //Binary search
 public class FindElement {
     public static void main(String[] args) {
-        int[]  arr = {1,2,3,6,5};
+        int[]  arr = {1,2,3,6,5, 6};
         int target = 6;
         System.out.println(getElement(arr, 0, target));
         System.out.println(findIndex(arr, 0, target));
+        System.out.println(findLastIndex(arr, arr.length -1,  target)); //last index
+        ArrayList<Integer> list = new ArrayList<>();
+      ArrayList<Integer> answ = findAllIndexes(arr, target, 0, list);
+        System.out.println(answ);
 
+    }
+
+    private static ArrayList<Integer> findAllIndexes(int[] arr, int target, int index, ArrayList<Integer> list) {
+     if(index == arr.length) {
+         return list;
+     }
+     if(target == arr[index]){
+         list.add(index);
+     }
+      return findAllIndexes(arr,target,index+1, list);
     }
 
     private static boolean getElement(int[] arr, int index, int target) {
@@ -24,4 +41,14 @@ public class FindElement {
         }
         return findIndex(arr, index + 1, target);
     }
+    private static int findLastIndex(int[] arr, int index, int target) {
+        if(index ==  -1){
+            return -1;
+        }
+        if(target == arr[index]){
+            return index;
+        }
+        return findIndex(arr, index -1, target);
+    }
+
 }
