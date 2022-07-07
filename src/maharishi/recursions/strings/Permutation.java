1cf82a720a6d55fc842028fc22dec62987625fcf
nahimanajz <jazzonahimana@gmail.com>
2022-07-07 10:48:50 +0200

    add permutation List

--- src/maharishi/recursions/strings/Permutation.java
+++ src/maharishi/recursions/strings/Permutation.java
@@ -1,2 +1,58 @@
-package maharishi.recursions.strings;public class Permutation {
+package maharishi.recursions.strings;
+
+import java.util.ArrayList;
+
+public class Permutation {
+    public static void main(String[] args) {
+        //permutations("", "abc");
+        ArrayList<String> pl = permutationList("", "abc");
+        System.out.println(pl);
+        System.out.println(countPermutation("", "abc"));
+    }
+
+    static void permutations(String newText, String text) {
+        if(text.isEmpty()){
+            System.out.println(newText);
+            return;
+        }
+        char character = text.charAt(0);
+
+        for (int i = 0; i <= newText.length(); i++) {
+            String first = newText.substring(0, i);
+            String second = newText.substring(i, newText.length());
+
+            permutations(first + character + second, text.substring(1));
+        }
+    }
+    static ArrayList<String> permutationList(String newText, String text) {
+        if(text.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+             list.add(newText);
+            return list;
+        }
+        char ch = text.charAt(0);
+        ArrayList<String> element = new ArrayList<>();
+
+        for (int i = 0; i <= newText.length() ; i++) {
+            String first = newText.substring(0, i);
+            String second = newText.substring(i, newText.length());
+            element.addAll(permutationList(first + ch + second, text.substring(1)));
+        }
+        return element;
+    }
+
+    static int countPermutation(String newText, String text) {
+        if (text.isEmpty()) {
+            return 1;
+        }
+        char character = text.charAt(0);
+        int count = 0;
+        for (int i = 0; i <= newText.length(); i++) {
+            String first = newText.substring(0, i);
+            String second = newText.substring(i, newText.length());
+
+            count = count + countPermutation(first + character + second, text.substring(1));
+        }
+        return count;
+    }
 }
--- src/maharishi/recursions/strings/Subset.java
+++ src/maharishi/recursions/strings/Subset.java
@@ -1,2 +1,64 @@
-package maharishi.recursions.strings;public class Subset {
+package maharishi.recursions.strings;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//ex:Input=>  arr = {1, 2,3} output = [[],[1], [2], [1, 2], [3],[1, 3], [2, 3], [1, 2, 3]]
+public class Subset {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3};
+        List<List<Integer>>  ans = subset(arr);
+        for (List<Integer> element : ans) {
+            System.out.println(element);
+        }
+        //Duplicated
+        for (List<Integer> elem: subsetWithDuplicates(arr)) {
+            System.out.print(elem +" ");
+        }
+    }
+
+     static List<List<Integer>> subset(int[] arr) {
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+
+        for(int num: arr) {
+            int n = outer.size();
+            for (int i = 0; i < n; i++) {
+                List<Integer> inner = new ArrayList<>(outer.get(i));
+                inner.add(num);
+                outer.add(inner);
+            }
+        }
+        return outer;
+    }
+    /**
+     * sort
+     * @return List<List<Integer>
+     */
+    static List<List<Integer>> subsetWithDuplicates(int[] arr){
+    Arrays.sort(arr);
+    List<List<Integer>> outer = new ArrayList<>();
+    outer.add(new ArrayList<>());
+    int start = 0;
+    int end = 0;
+        for (int i = 0; i < arr.length; i++) {
+            start = 0;
+            //increment when current == previous element
+            if(i > 0 && arr[i] == arr[i-1]) {
+                start = end +1;
+            }
+            end = outer.size() -1;
+            int n = outer.size();
+            for (int j = start; j < n; j++) {
+                List<Integer> internal = new ArrayList<>(outer.get(j));
+                internal.add(arr[i]);
+                outer.add(internal);
+
+            }
+
+        }
+        return  outer;
+
+    }
 }
