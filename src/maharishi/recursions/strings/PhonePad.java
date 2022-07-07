c2f0ae5a6331dc1e62c4c51e7f79180360297603
nahimanajz <jazzonahimana@gmail.com>
2022-07-07 14:04:52 +0200

    genereate appropiote pad in array list

--- src/maharishi/recursions/strings/PhonePad.java
+++ src/maharishi/recursions/strings/PhonePad.java
@@ -1,9 +1,13 @@
 package maharishi.recursions.strings;
 // https://leetcode.com/problems/letter-combinations-of-a-phone-number/
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PhonePad {
     public static void main(String[] args) {
         pad("12", "");
+        System.out.println(padInList("12", ""));
     }
 
      static void pad(String number, String letterCombination) {
@@ -11,13 +15,29 @@ public class PhonePad {
             System.out.println(letterCombination);
             return;
         }
-        int digit = number.charAt(0) - '0';
+        int digit = number.charAt(0) - '0'; // convert character to number
 
-        for (int i = (digit - 1) * 3; i < digit * 3; i++) {
+        for (int i = (digit - 1) * 3; i < digit * 3; i++) { // since phone pad has three characters then multiply by three
             char alphabet = (char) ('a' + i); // get alphabetical character
             pad(number.substring(1), letterCombination + alphabet);
 
         }
+    }
+    static List<String> padInList(String number, String letterCombination) {
+        if(number.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(letterCombination);
+            return  list;
+
+        }
+        int digit = number.charAt(0) - '0'; // convert character to number
+        ArrayList<String> list = new ArrayList<>();
+
+        for (int i = (digit - 1) * 3; i < digit * 3; i++) { // since phone pad has three characters then multiply by three
+            char alphabet = (char) ('a' + i); // get alphabetical character
+            list.addAll(padInList(number.substring(1), letterCombination + alphabet));
+        }
+        return  list;
     }
 
 }
