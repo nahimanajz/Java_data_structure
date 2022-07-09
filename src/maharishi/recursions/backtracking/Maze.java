f2de5a47b4dca836ad7e152fa1b0d7c824986196
janvierbwk <na-janvier@bw-africa.com>
2022-07-09 10:10:41 +0200

    (recursion): Add 3X3 maze problem and diagnals

--- src/maharishi/recursions/backtracking/Maze.java
+++ src/maharishi/recursions/backtracking/Maze.java
@@ -1,8 +1,12 @@
 package maharishi.recursions.backtracking;
 
+import java.util.ArrayList;
+
 public class Maze {
     public static void main(String[] args) {
         System.out.println(maze(3, 3));
+        mazePath("", 3,3);
+        System.out.println(diagonal("", 3, 3));
     }
     static int maze(int row, int col){
         if(row == 1 || col == 1){
@@ -11,5 +15,34 @@ public class Maze {
         int right = maze(row, col-1);
         int down = maze(row-1, col);
         return right + down;
+    }
+    static  void mazePath(String path, int col, int row) {
+        if(row == 1 &&  col == 1){
+            System.out.println(path);
+            return;
+        }
+        if(row > 1) mazePath(path + 'D', col, row -1);
+        if(col > 1) mazePath(path + 'R', col -1, row);
+
+    }
+    static ArrayList<String> diagonal(String path, int col, int row){
+        if(row == 1 && col == 1){
+         ArrayList<String> ans = new ArrayList<>();
+         ans.add(path);
+         return ans;
+        }
+        ArrayList<String> list = new ArrayList<>();
+       if(row > 1 && col > 1){
+           list.addAll(diagonal(path+"D", col -1, row -1)); // diagonal
+       }
+       if(row > 1){
+           list.addAll(diagonal(path+"V", col, row -1)); //down
+       }
+        if (col > 1) {
+            list.addAll(diagonal(path+"H", col -1, row)); // right
+
+        }
+        return list;
+
     }
 }
